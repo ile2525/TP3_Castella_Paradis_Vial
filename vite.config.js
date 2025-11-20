1ddfeb875ba300b729127f9160502d5ecf1959d1
Ileona Vial <2405565@bdeb.qc.ca>
2025-11-20 16:41:42 -0500

    mise en ligne

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+ 
+export default defineConfig({
+    base: '/TP3_Castella_Paradis_Vial/',
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                //...mettre toutes les autres pages
+            },
+        },
+    },
+});
+ 
