f1b0d63205f80a2b7c9691741c2f2b0d0d3113b2
Ileona Vial <2405565@bdeb.qc.ca>
2025-11-27 17:29:41 -0500

    Importation gsap

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to('.square', {
+	x: 1000,
+    duration : 5,
+})
+
 import './style.css'
