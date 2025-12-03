e4eedbb51875eeef178a3bd8b6405217798c427f
ileona <2405565@bdeb.qc.ca>
2025-12-02 21:34:43 -0500

    Exemple de configuration pour ScrollTrigger

--- src/main.js
+++ src/main.js
@@ -3,9 +3,22 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.to('.square', {
-	x: 1000,
-    duration : 5,
+// TUTO
+// Voici un exemple de fonctionnement :
+
+gsap.to('.part3-nuee', { // Ne pas oublier d'indiquer ta classe css
+
+    // Propriétés d'animation
+    opacity: 1, // Finit en opacité 1
+    y: 1000, // Permet l'animation vers le bas
+   
+    // Conditions de déclanchements
+    scrollTrigger: {
+    trigger: ".part3", //Le contenu de la classe devient la "zone de surveillance"
+    start: "top+=500", // L'animation commence à 500px du haut de ".part3"
+    end: "bottom", // Et s'arrete en bas de la classe
+    scrub: 2 // relie l'animation au scroll, ici 2 permet un effet "tu descends, je te rattrape"
+    }
 })
 
 import './style.css'
