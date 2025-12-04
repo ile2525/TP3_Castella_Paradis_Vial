81f7372131d5697c2e1e96cee0ba1158de758489
Ileona Vial <2405565@bdeb.qc.ca>
2025-12-04 17:42:40 -0500

    Ajustements

--- src/main.js
+++ src/main.js
@@ -10,14 +10,17 @@ gsap.to('.part3-nuee', { // Ne pas oublier d'indiquer ta classe css
 
     // Propriétés d'animation
     opacity: 1, // Finit en opacité 1
-    y: 1000, // Permet l'animation vers le bas
+    y: 100, // Permet l'animation vers le bas
+
+   
    
     // Conditions de déclenchements
     scrollTrigger: {
     trigger: ".part3", //Le contenu de la classe devient la "zone de surveillance"
-    start: "top+=500", // L'animation commence à 500px du haut de ".part3"
-    end: "bottom", // Et s'arrete en bas de la classe
-    scrub: 2 // relie l'animation au scroll, ici 2 permet un effet "tu descends, je te rattrape"
+    start: "top+=1080", // L'animation commence à 500px du haut de ".part3"
+    end: "bottom bottom", // Et s'arrete en bas de la classe
+    scrub: 2, // relie l'animation au scroll, ici 2 permet un effet "tu descends, je te rattrape"
+    markers: true
     }
 })
 
