0368c2880927f62505e125e04406a9730cd32db6
ileona <2405565@bdeb.qc.ca>
2025-12-10 12:32:45 -0500

    Pluie de cendre - une galereeeee

--- src/scroll-trigger.js
+++ src/scroll-trigger.js
@@ -1,8 +1,9 @@
+import { spawnCendre } from "./animation.js";
+
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-
 
 
 export function scrollAnimation() {
@@ -42,5 +43,36 @@ export function scrollAnimation() {
         }
 
     })
+
+    // -------------------- Section 4 - Ilé --------------------
+
+    const particuleContainer = document.getElementById("particule-container");
+    const cendreMontante = document.getElementById("cendre-montante");
+    
+    ScrollTrigger.create({
+        trigger: ".partie4",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+        markers: true,
+
+        onUpdate: (self) => {
+            // Génération de cendre au scroll
+            const particule = spawnCendre(particuleContainer);
+
+            gsap.from(particule, {
+                y: -200,
+                opacity: 0,
+                duration: 2,
+                ease: "none"
+            });
+
+            // montée du tas de cendres
+            const monte = self.progress; // 0 -> 1
+           cendreMontante.style.height = (monte * 100) + "vh";
+        }
+    });
+
+
 }
 
