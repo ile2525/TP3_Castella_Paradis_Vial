8523620bc1a8bf788b33e72e70db14a066b50945
Ileona Vial <2405565@bdeb.qc.ca>
2025-12-11 15:09:43 -0500

    page

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'index.html',
-                page1: 'pompei.html',
+                pompei: 'pompei.html'
                 //...mettre toutes les autres pages
             },
         },
