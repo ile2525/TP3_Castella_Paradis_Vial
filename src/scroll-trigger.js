10c695c573d3e3da4a25c9560915fd6e20702f05
ChoupiPlanete <1064152@bdeb.qc.ca>
2025-12-10 21:02:09 -0500

    section1 - balcon

--- src/scroll-trigger.js
+++ src/scroll-trigger.js
@@ -22,7 +22,7 @@ export function scrollAnimation() {
             start: "top 10%",
             end: "+=625% 20%",
             scrub: true,
-            markers: true
+            //markers: true
         }
     })
     //-----apparition plan 3----------------------------------------------------------
@@ -34,34 +34,49 @@ export function scrollAnimation() {
             start: "top bottom",
             end: "bottom bottom",
             scrub: true,
-            markers: true
+            //markers: true
         }
     })
 
-//-----apparition plan 2 ----------------------------------------
-    gsap.to(".v2", {
+//-----apparition plan 2 et narration 2----------------------------------------
+    gsap.to([".v2", ".nar2"], {
         opacity: 1,
 
         scrollTrigger: {
             trigger: ".boxBottom",
             start: "+=50% bottom",
-            end: "bottom bottom",
+            end: "+=10% bottom",
             scrub: true,
-            markers: true
+            //markers: true
         }
     })
     //-----apparition premier plan ------------------
         gsap.to(".v3", {
         opacity: 1,
+        scale: 1,
 
         scrollTrigger: {
             trigger: ".boxBottom",
-            start: "+=80% bottom",
+            start: "+=70% 95%",
             end: "bottom bottom",
             scrub: true,
-            markers: true
+            //markers: true
         }
     })
+    //-----fadeout-------------------------
+    gsap.to(".fadeout", {
+  opacity: 0.7,
+
+  scrollTrigger: {
+    trigger: ".v3",
+    start: "50% 70%", 
+    end: "-=70% bottom",
+    scrub: true,
+    markers: true
+  }
+})
+//-----narration3 sur fadeout----------------
+
 
 
 
